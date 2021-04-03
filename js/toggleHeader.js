fe4a3f60e188f8028b7ae74d350c35f8cb4fff83
Lasse Aakjær <51378679+TheLoGgI@users.noreply.github.com>
2021-04-04 00:03:42 +0200

    Responsive header nav, mobil and desktop

--- js/toggleHeader.js
+++ js/toggleHeader.js
@@ -5,10 +5,10 @@ menuIcon.addEventListener('click', () => {
     toggleMenuNavgation()
 })
 
-function toggleMenuNavgation(show = false) {
+function toggleMenuNavgation(show) {
     const mobilNav = document.getElementById('mobilNavigationToggle')
     const shadow = document.getElementById('shadowNav')
-    isMobilNavigationShowing = !isMobilNavigationShowing
+    isMobilNavigationShowing = show ?? !isMobilNavigationShowing // '??' Nullish coalescing operator - if not null or undefined use the show variable
     if (isMobilNavigationShowing) {
         mobilNav.style.transform = `translateX(20%)`
         shadow.style.transform = `translateX(0%)`
@@ -21,3 +21,22 @@ function toggleMenuNavgation(show = false) {
     }
 
 }
+
+window.addEventListener('resize', () => {
+
+    const mobilNav = document.getElementById('mobilNavigationToggle')
+    if (window.innerWidth >= 1200) {
+        // mobilNav.style.transition = 'transform 0.3s ease-in-out 0s'
+        mobilNav.style.transform = `translateX(0%)`
+    } else {
+        mobilNav.style.transition = 'all 0s ease 0s'
+        mobilNav.style.transform = `translateX(110%)`
+        
+        // Prevents 1 tick animation
+        setTimeout(() => {
+            mobilNav.style.transition = 'transform 0.3s ease-in-out 0s'
+        }, 0)
+    }
+
+
+})
