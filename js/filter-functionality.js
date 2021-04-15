3cf040f93868674cb35d6b0ca5e9e9781686bd28
Emil Bruun Larsen <60384288+Emilovich98@users.noreply.github.com>
2021-04-15 09:17:35 +0200

    Start på javascript filtermenu

--- js/filter-functionality.js
+++ js/filter-functionality.js
@@ -1,8 +1,38 @@
 // mobile filter show and hide function
 
-let openFilterBtn = document.getElementById("filter-button");
-let closeFilterBtn = document.querySelector(".mobile-filter-close-btn");
+let filterBGShadow = document.querySelector(".mobile-filter-bg-fill");
 
-console.log("hej");
-console.log(openFilterBtn);
-console.log(closeFilterBtn);
+function showFilterWindow() {
+
+    console.log("hej");
+    let filterContainer = document.querySelector(".mobile-filter-container");
+
+    filterContainer.style.display = "block";
+    filterBGShadow.style.opacity = "1";
+}
+
+function hideFilterWindow() {
+
+    console.log("farvel");
+    let filterContainer = document.querySelector(".mobile-filter-container");
+
+    filterContainer.style.display = "none";
+    filterBGShadow.style.opacity = "0";
+}
+
+// expand categori of filter mobile
+
+function expandMobileFilterType() {
+
+    let filterList = document.querySelector(".filter-subject-list");
+    let filterArrow = document.querySelector(".filter-subject-arrow");
+    console.log("expand");
+
+    if (filterList.style.height === "auto") {
+        filterList.style.height = "0px";
+        filterArrow.style.transform = "rotate(90deg)";
+
+    } else {
+        filterList.style.height = "auto";
+    }
+}
